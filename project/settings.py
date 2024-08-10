f30f08f0e8a5df62e540a436b1195fe1fb1bf339
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2024-08-10 10:09:55 -0500

    fix bug, send empty tasks

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dlc*$=ei*-#6&!&*e*u_g43)+tyb!7p1qxm0=ece(j#^e!7rip'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
